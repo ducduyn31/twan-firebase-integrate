54be17003ffb9724d4af48f7d5e05fb44207d8db
Duy Nguyen <ducduy.ababat@gmail.com>
2021-07-19 09:27:49 +0700

    new package

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,6 @@
-import {Subject} from 'rxjs';
-import {map} from 'rxjs/operators';
-
 export const Config = {
     KAFKA_BROKER: undefined,
     KAFKA_CREATE_USER_TOPIC: undefined,
     FIREBASE_DATABASE_URL: undefined,
     KAFKA_GET_USER: undefined,
 };
-
-export const configInitTrigger = new Subject<any>();
-
-export const onConfigInitialized = (dependency, ...args) => configInitTrigger.pipe(map(() => {
-    console.log(dependency(...args))
-    return dependency(...args);
-})).toPromise();
